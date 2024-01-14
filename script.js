d8a93760c8c9d8cef38cdf87d254929e80f7c6aa
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2024-01-14 11:35:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const arr = [1,2,3,4]
+const promise = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve(arr)
+	},3000)
+})
+function abc(){
+	return promise
+}
+x.then((arr)=>{
+	const oddArr =arr.filter((num)=>{
+		return parseInt(num%2)===1
+	})
+	const div = document.getElementById("output")
+	div.innerText = oddArr;
+})
+
+x.then((arr)=>{
+	const newArr =arr.map((num)=>{
+		if(parseInt(num%2)===0){
+			return num*2
+		}
+	})
+	const div = document.getElementById("output")
+	div.innerText = newArr;
+})
+
+const x = abc()
